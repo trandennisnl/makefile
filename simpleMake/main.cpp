aefe6fd72bd193abbd3516e96730cd5686267c4d
Dennis Tran <trandennis.nl@gmail.com>
2019-05-14 13:56:53 -0700

    Learn how to make simple makefile.

---
+++ simpleMake/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // Preprocessor directive indicates use of iostream library
+
+// Every C++ program must have a special function named main (all lower case letters).
+// When the program is run, execution starts with the first statement inside of function main and then continues sequentially.
+int main()
+{
+    // Send "Hello World" to console output (cout)
+    std::cout << "Hello World";
+    return 0;
+}
